1fdcdca3c4da6e50c21161f83aed6848d47c2f34
Ekaterina Gerasina <gerasinaes@mail.ru>
2020-12-04 16:06:10 +0300

    .

---
+++ src/sample/logic/Board.java
@@ -0,0 +1,118 @@
+package sample.logic;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.Math.abs;
+
+public class Board {
+    private int[][] tile;
+    private int zeroX;
+    private int zeroY;
+    private int h;
+
+
+    public Board(int[][] tile) {
+        this.tile = deepCopy(tile);
+
+        h = 0;
+        for (int i = 0; i < 4; i++) {  //в этом цикле определяем координаты нуля и вычисляем h(x)
+            for (int j = 0; j < 4; j++) {
+
+                if (tile[i][j] != (i * 4 + j + 1) && tile[i][j] != 0) {
+                    for (int k = 1; k < 16; k++) {
+                        if (k == tile[i][j]) {  // номер на ячейке от 1 до 16
+                            int y0 = (k - 1) / 4;
+                            int x0 = k - y0 * 4 - 1;
+                            h += (abs(i - x0) + abs(j - y0));
+                        }
+
+                    }
+                }
+                if (tile[i][j] == 0) {
+                    zeroX = i;
+                    zeroY = j;
+                }
+            }
+        }
+
+        System.out.println("h" + h);
+        System.out.println("x" + zeroX);
+        System.out.println("Y" + zeroY);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (tile[i][j] != board.tile[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    @Override
+    public int hashCode() { //deepHashCode проверяет вложенные массивы на совпадение номеров элементов и содержимого
+        return Arrays.deepHashCode(tile);
+    }
+
+
+    public int getH() {
+        return h;
+    }
+
+    public boolean checkWin() {  //   если мера неупорядоченности = -> терминальная позиция
+        return h == 0;
+    } //мера неупорядоченности = 0 -> все ячейки на терминальных позициях
+
+
+
+    public Iterable<Board> neighbors() {  //порождаем дочерние состояния
+
+        Set<Board> boardList = new HashSet<Board>();
+        boardList.add(changePositions(getNewBlock(), zeroX, zeroY, zeroX, zeroY + 1));
+        boardList.add(changePositions(getNewBlock(), zeroX, zeroY, zeroX, zeroY - 1));
+        boardList.add(changePositions(getNewBlock(), zeroX, zeroY, zeroX - 1, zeroY));
+        boardList.add(changePositions(getNewBlock(), zeroX, zeroY, zeroX + 1, zeroY));
+
+        return boardList;
+    }
+
+
+
+
+    private Board changePositions(int[][] blocks2, int x1, int y1, int x2, int y2) {  //  в этом методе меняем два соседних поля
+        if (x2 > -1 && x2 < 4 && y2 > -1 && y2 < 4) {
+            int t = blocks2[x2][y2];
+            blocks2[x2][y2] = blocks2[x1][y1];
+            blocks2[x1][y1] = t;
+            return new Board(blocks2);
+        } else
+            return null;
+    }
+
+    private int[][] getNewBlock() {
+        return deepCopy(tile);
+    }
+
+    private static int[][] deepCopy(int[][] original) { // копия поля, чтобы состояние было неизменным тк иначе поле изменится в changePositions
+        if (original == null) {
+            return null;
+        }
+
+        final int[][] result = new int[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            result[i] = new int[original[i].length];
+            System.arraycopy(original[i], 0, result[i], 0, original[i].length);
+        }
+        return result;
+    }
+
+
+}
---
+++ src/sample/logic/Solver.java
@@ -0,0 +1,101 @@
+package sample.logic;
+
+import java.util.*;
+
+public class Solver {
+
+
+    private List<Board> path = new ArrayList<>();   // этот лист - цепочка ходов, приводящих к решению задачи
+
+    private static class Item {    //класс - сслылка на предыдущие состояния поля
+        private Item predecessor;  // ссылка на предыдущее
+        private Board board;   // сама позиция
+
+        private Item(Item predecessor, Board board) {
+            this.predecessor = predecessor;
+            this.board = board;
+        }
+
+        public Board getBoard() {
+            return board;
+        }
+    }
+
+
+    public Solver(Board primaryCondition) {
+
+        Comparator<Item> comp = Comparator.comparingInt(Solver::fx);
+        //  очередь. Для нахождения приоритетного сравниваем меры,min Мера = max Приоритет
+        PriorityQueue<Item> priorityQueue = new PriorityQueue<>(comp);
+
+
+        // добавляем исходное поле
+        priorityQueue.add(new Item(null, primaryCondition));
+
+        for (;;) {
+            Item board = priorityQueue.poll();
+
+            //   если дошли до решения, сохраняем весь путь ходов в лист
+            assert board != null;
+            if (board.board.checkWin()) {
+                insertInPath(new Item(board, board.board));
+                return;
+            }
+
+
+
+            for (Board neighbor : board.board.neighbors()) {
+                // один из соседей - это позиция которая была ходом раньше
+
+                if (neighbor != null && !wasInPath(board, neighbor)) //если уже не содержится в пути(списке рассмотренных)
+                    priorityQueue.add(new Item(board, neighbor));
+            }
+
+        }
+    }
+
+    //  f(x) = h+g
+    private static int fx(Item item) {
+        Item itemCurr = item;
+        int gx = 0;
+        int hx = item.getBoard().getH();
+        for (;;) {
+            gx++;
+            itemCurr = itemCurr.predecessor;
+            if (itemCurr == null) {
+                return hx + gx;
+            }
+        }
+    }
+
+
+    private void insertInPath(Item item) {
+        Item item2 = item;
+        for (;;) {
+            item2 = item2.predecessor;
+            if (item2 == null) {
+                return;
+            }
+            path.add(0, item2.board); //добавляем в начало
+        }
+    }
+
+    // была ли уже такая позиция в пути
+    private boolean wasInPath(Item item, Board board) {
+        Item item2 = item;
+        for (;;) {
+            if (item2.board.equals(board)) return true;
+            item2 = item2.predecessor;
+            if (item2 == null) return false;
+        }
+    }
+
+
+    public Iterable<Board> solution() {
+        System.out.println(path.size());
+        return path;
+
+    }
+
+
+}
